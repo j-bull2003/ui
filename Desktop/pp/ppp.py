a303313a577f446761d3a70f0963f4a1ec0cf9d6
j-bull2003 <117891001+j-bull2003@users.noreply.github.com>
2023-03-24 16:38:56 +0000

    m

--- Desktop/pp/ppp.py
+++ Desktop/pp/ppp.py
@@ -4,6 +4,6 @@ lbs = float(weight)/0.45
 kg = float(weight)*2.2
 
 if measurement == "K":
-    print(float(lbs) + str("lbs"))
+    print(lbs + "lbs")
 elif measurement == "L":
-    print(float(kg) + str("kg"))
+    print(kg + "kg")
